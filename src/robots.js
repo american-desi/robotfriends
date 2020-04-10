ff7d667eaa94c52d30fc23515224e227f326fcc5
american-desi <joshuathomas016@gmail.com>
2020-04-10 00:28:14 -0400

    first commit

---
+++ src/robots.js
@@ -0,0 +1,83 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Joshua Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 2,
+	name: 'Nick Thomas',
+	username: 'nickthomas',
+	email: 'nick@gmail.com'
+
+},
+{
+	id: 3,
+	name: 'Adam Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 4,
+	name: 'Noah Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 5,
+	name: 'Shaji Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 6,
+	name: 'Subha Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 7,
+	name: 'Sheba Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 8,
+	name: 'Jeremy Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 9,
+	name: 'Nicole Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+{
+	id: 10,
+	name: 'Jessica Thomas',
+	username: 'joshthomas',
+	email: 'josh@gmail.com'
+
+},
+
+
+
+
+
+
+
+
+
+
+
+]
