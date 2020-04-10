5dcd44b4132042c3de172758d2c8212853a09cdd
american-desi <joshuathomas016@gmail.com>
2020-04-10 08:42:55 -0400

    Updates

---
+++ precache-manifest.ea73f26131c8216a02f15b6554666bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7337afc4de2b487b8632846a30dae6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b3c43ec17caf4c658306",
+    "url": "/static/css/main.457e7235.chunk.css"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/static/js/2.f68723af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f68723af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c43ec17caf4c658306",
+    "url": "/static/js/main.2174995f.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
